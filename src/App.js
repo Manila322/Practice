e0d59c1e7806bb864b357ab934e2e739ae694ca5
Данил Пронькин <danilpronkin@MacBook-Air-Danil.local>
2024-11-26 14:15:19 +0300

    Тест

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+export const App = () => {
+	return (
+		<div>
+			<i className="fa fa-bath" aria-hidden="true"></i>
+			<div>dasdsa</div>
+			<Div>dsadsa</Div>
+		</div>
+	);
+};
