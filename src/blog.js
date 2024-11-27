9b796cca5bebe82d29fc7896ee4073d64a91ac4c
Pronkin Danil <zzzomb4e@mail.ru>
2024-11-27 19:37:53 +0300

    Реализовать основной компонент с роутингом

---
+++ src/blog.js
@@ -0,0 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Content = styled.div`
+	padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+	text-align: center;
+`;
+
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Футер</div>;
+
+export const Blog = () => {
+	return (
+		<>
+			<Header />
+			<Content>
+				<H2>Контент страницы</H2>
+				<Routes>
+					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/login" element={<div>Страница авторизации</div>} />
+					<Route path="/register" element={<div>Страница регистрации</div>} />
+					<Route path="/users" element={<div>Пользователи</div>} />
+					<Route path="/post" element={<div>Новая статья</div>} />
+					<Route path="/post/:postID" element={<div>Статья</div>} />
+					<Route path="*" element={<div>Ошибка</div>} />
+				</Routes>
+			</Content>
+			<Footer />
+		</>
+	);
+};
